c0dfc1bd794293fcdce94c1915f4024b66992190
manishk_sharma@outlook.com <manishk_sharma@outlook.com>
2023-08-17 07:26:01 +0530

    commit

---
+++ Areas/Identity/Pages/_Route_Layout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechyRecruit.Areas.Identity.Pages;
+
+public class _Route_Layout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
